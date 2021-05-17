ba03007afc477b00a42a5d495dc284716714026c
BladeBTW <52164618+BladeBTW@users.noreply.github.com>
2021-05-17 16:42:00 +0200

    Fixed StartupTitle issue

--- runelite-client/src/main/java/net/runelite/client/plugins/autoaccount/AutoAccountPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/autoaccount/AutoAccountPlugin.java
@@ -3,12 +3,15 @@ package net.runelite.client.plugins.autoaccount;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.game.WorldService;
 import net.runelite.client.input.KeyListener;
 import net.runelite.client.input.KeyManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.defaultworld.DefaultWorldPlugin;
+import net.runelite.client.ui.ClientUI;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.inject.Inject;
@@ -18,83 +21,125 @@ import java.awt.event.KeyEvent;
 import static java.awt.event.KeyEvent.VK_ENTER;
 
 @PluginDescriptor(
-    name = "Auto login",
-    description = "Allows you to auto login via CLI args"
+				name = "Auto login",
+				description = "Allows you to auto login via CLI args"
 )
 @Slf4j
-public class AutoAccountPlugin extends Plugin implements KeyListener {
+public class AutoAccountPlugin extends Plugin implements KeyListener
+{
 
-  @Inject
-  @Named("username")
-  private String username;
+	@Inject
+	@Named("username")
+	private String username;
 
-  @Inject
-  @Named("password")
-  private String password;
+	@Inject
+	@Named("password")
+	private String password;
 
-  @Inject
-  @Named("world")
-  private String world;
+	@Inject
+	@Named("world")
+	private String world;
 
-  @Inject
-  private Client client;
+	@Inject
+	@Named("xPos")
+	private String xPos;
 
-  @Inject
-  private WorldService worldService;
+	@Inject
+	@Named("yPos")
+	private String yPos;
 
-  @Inject
-  private KeyManager keyManager;
+	@Inject
+	@Named("startupTitle")
+	private String startupTitle;
 
-  private boolean loggedIn;
+	@Inject
+	private ClientUI clientUI;
 
-  @Override
-  protected void startUp() throws Exception
-  {
-    keyManager.registerKeyListener(this);
-  }
+	@Inject
+	private Client client;
 
-  @Override
-  protected void shutDown() throws Exception
-  {
-    keyManager.unregisterKeyListener(this);
-  }
+	@Inject
+	private WorldService worldService;
 
-  @Override
-  public void keyTyped(KeyEvent e) {
+	@Inject
+	private KeyManager keyManager;
 
-  }
+	private boolean loggedIn;
 
-  @Override
-  public boolean isEnabledOnLoginScreen()
-  {
-    return true;
-  }
+	@Override
+	protected void startUp() throws Exception
+	{
+		keyManager.registerKeyListener(this);
+		if (StringUtils.isNotEmpty(startupTitle))
+		{
+			clientUI.getFrame().setTitle(startupTitle);
+		}
+	}
 
-  @Override
-  public void keyPressed(KeyEvent e) {
-    if (client.getGameState() != GameState.LOGIN_SCREEN || e.getKeyCode() != VK_ENTER) {
-      return;
-    }
+	@Override
+	protected void shutDown() throws Exception
+	{
+		keyManager.unregisterKeyListener(this);
+	}
 
-    if (StringUtils.isNotEmpty(username)) {
-      client.setUsername(username);
-    }
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	{
+		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		{
+			if (StringUtils.isNotEmpty(xPos) && StringUtils.isNotEmpty(yPos))
+			{
+				int x = Integer.parseInt(xPos);
+				int y = Integer.parseInt(yPos);
+				clientUI.getFrame().setLocation(x, y);
+			}
+		}
+	}
 
-    if (StringUtils.isNotEmpty(password)) {
-      client.setPassword(password);
-    }
+	@Override
+	public boolean isEnabledOnLoginScreen()
+	{
+		return true;
+	}
 
-    if (StringUtils.isNotEmpty(world)) {
-      int newWorld = Integer.parseInt(world);
-      if (newWorld != client.getWorld()) {
-        DefaultWorldPlugin.changeWorld(newWorld, client, worldService);
-      }
+	@Override
+	public void keyTyped(KeyEvent e)
+	{
 
-    }
-  }
+	}
 
-  @Override
-  public void keyReleased(KeyEvent e) {
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+		if (client.getGameState() != GameState.LOGIN_SCREEN || e.getKeyCode() != VK_ENTER)
+		{
+			return;
+		}
 
-  }
+		if (StringUtils.isNotEmpty(username))
+		{
+			client.setUsername(username);
+		}
+
+		if (StringUtils.isNotEmpty(password))
+		{
+			client.setPassword(password);
+		}
+
+		if (StringUtils.isNotEmpty(world))
+		{
+			int newWorld = Integer.parseInt(world);
+			if (newWorld != client.getWorld())
+			{
+				DefaultWorldPlugin.changeWorld(newWorld, client, worldService);
+			}
+
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e)
+	{
+
+	}
 }
